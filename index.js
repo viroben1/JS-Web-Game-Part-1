1b9632931ff5dab3c5a3033a7505114943b875c6
benjamin farmer <viroben2001@gmail.com>
2022-10-01 15:06:42 -0700

    bonus final code

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ function newImage(url, left, bottom){
     document.body.append(object)
     return object
 }
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
 
 function newItem(url, left, bottom){
     let object = newImage(url, left, bottom)
